7187fef59914c01efe34b78c5a4315bec0c8d505
Kelve <kelvearagao@gmail.com>
2014-12-11 13:49:30 -0300

    Consolidating code using the event context.

--- chapter-03/01.js
+++ chapter-03/01.js
@@ -1,22 +1,22 @@
 
 $(document).ready(function() {
-	$('#switcher-default')
-		.addClass('selected')
-		.on('click', function() {
-		  $('body').removeClass('narrow').removeClass('large');
-	});
-	
-	$('#switcher-narrow').on('click', function() {
-		$('body').addClass('narrow').removeClass('large');
-		
-	});
-	
-	$('#switcher-large').on('click', function() {
-		$('body').removeClass('narrow').addClass('large');
-	});
+	$('#switcher-default').addClass('selected');
 	
 	$('#switcher button').on('click', function() {
+		var bodyClass = this.id.split('-')[0];
+		
+		$('body').removeClass().addClass(bodyClass);
 		$('#switcher button').removeClass('selected');
 		$(this).addClass('selected');
 	});
+	
+	/*
+	$('#switcher-narrow').on('click', function() {
+		$('body').addClass('narrow');
+	});
+	
+	$('#switcher-large').on('click', function() {
+		$('body').addClass('large');
+	});
+	*/
 });
